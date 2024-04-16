9bb227eeb97b52b4abfdda07f9217098fd472a6a
mayspiek <maya.spieker@gmail.com>
2024-04-16 11:55:20 -0300

    STYLES: alterando estruturação do html e arrumando estilização

--- src/components/Aside/index.styles.ts
+++ src/components/Aside/index.styles.ts
@@ -18,33 +18,37 @@ const AsideStyles = styled.aside`
     }
     ul{
         display: flex;
+        width: 100%;
         flex-direction: column;
         gap: ${Theme.margins.margin1rem};
-        padding: ${Theme.margins.margin1rem};
+        padding: ${Theme.margins.marginhalfrem};
 
         width: 100%;
 
-        li {
-            list-style: none;
+        a {
             display: inherit;
-            align-items: center;
+            list-style: none;
+            text-decoration: none;
+
             gap: ${Theme.margins.marginhalfrem};
-
+            
             padding: ${Theme.margins.margin5px};
-
+            
             &:hover{
                 background-color: #FFAE47;
                 color: ${Theme.colors.orange};
                 border-radius: ${Theme.borders.radius};
             }
+            li {
+                display: inherit;
+                align-items: center;
 
-            a {
-                text-decoration: none;
                 color: ${Theme.colors.light};
                 font-size: ${Theme.font.sizes.xsmall};
                 font-weight: ${Theme.font.weight.regular};
                 font-family: ${Theme.font.family.poppins};
             } 
+
 
             img {width: 48px;}
         }
