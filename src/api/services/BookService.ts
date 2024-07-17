ab531f296bbca600c46c9f3a2e3c11b17cd19dcb
mayspiek <maya.spieker@gmail.com>
2024-07-17 15:47:34 -0300

    paginaçao e afins

--- src/api/services/BookService.ts
+++ src/api/services/BookService.ts
@@ -9,8 +9,8 @@ export class BookService {
         this.client = new ShelfieHttpClient();
     }
 
-    async postBookStatus({ googleId, status }: BookData) : Promise<HttpResponse<any>> {
-        const newStatus = (status ?? '').toUpperCase().replace(' ', '_');
+    async postBookStatus({ googleId, bookStatus }: BookData): Promise<HttpResponse<any>> {
+        const newStatus = (bookStatus ?? '').toUpperCase().replace(' ', '_');
 
         const base = `/api/mybooks/${googleId}/${newStatus}`;
 
@@ -19,20 +19,20 @@ export class BookService {
         return response;
     }
 
-    async postProgression({ progression, id }: BookData) : Promise<HttpResponse<any>> {
+    async postProgression({ progression, id }: BookData): Promise<HttpResponse<any>> {
         const base = `/api/mybooks/reading`;
 
         const data = {
             commentary: progression?.commentary,
             pages: progression?.pages,
             myBooksId: id
-        } 
+        }
 
         const response = await this.client.post({
             url: base,
             body: data
         });
-        
+
         if (response.statusCode === StatusCode.Created) {
             return {
                 ...response,
@@ -41,6 +41,64 @@ export class BookService {
         } else return {
             ...response,
             reject: 'Erro ao salvar progressão',
+        }
+    }
+
+    async fetchBooksByUser(): Promise<HttpResponse<any>> {
+        const base = '/api/mybooks';
+
+        const response = await this.client.get({ url: base });
+
+        if (response.statusCode === StatusCode.Ok) {
+            return {
+                statusCode: StatusCode.Ok,
+                body: response.body,
+                resolve: 'Sucesso ao buscar livros',
+            }
+
+        } else {
+            return {
+                ...response,
+                reject: 'Erro ao buscar livros',
+            };
+        }
+    }
+
+    async fetchBooksByStatus(status: string): Promise<HttpResponse<any>> {
+        const base = `/api/mybooks/status/${status}`;
+
+        const response = await this.client.get({ url: base });
+
+        if (response.statusCode === StatusCode.Ok) {
+            return {
+                statusCode: StatusCode.Ok,
+                body: response.body,
+                resolve: 'Sucesso ao buscar livros',
+            }
+
+        } else {
+            return {
+                ...response,
+                reject: 'Erro ao buscar livros',
+            };
+        }
+    }
+
+    disableBook = async (myBooksId: string): Promise<HttpResponse<any>> => {
+        const base = `/api/mybooks/${myBooksId}/disable`;
+
+        const response = await this.client.put({ url: base });
+
+        if (response.statusCode === StatusCode.Ok) {
+            return {
+                statusCode: StatusCode.Ok,
+                resolve: 'Livro desativado com sucesso',
+            }
+        } else {
+            return {
+                ...response,
+                reject: 'Erro ao desativar livro',
+            }
         }
     }
 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,33 +1,130 @@
 import { BookResume } from "../../components/BookResume/book-resume.tsx";
 import { Layout } from "../layout/index.js";
-import { Carrousel, CarrouselTitle, EmptyCarrousel } from "./index.style.ts";
-import { useGetUserData } from "../../api/hooks/useGetUserData.ts";
-import { useBookSearch } from "../../api/hooks/useBookSearch.ts";
-
-// to do - lista de livros e seus tratamentos
+import { Carrousel, BooksWrapper, TabListStyle, TabStyle } from "./home.style.ts";
+import { useFetchBooksByUser } from "../../api/hooks/useFetchBooksByUser.ts";
+import { Pagination, Tab } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { ChangeEvent, useState } from "react";
+import { BookData } from "../../types/bookData.ts";
 
 export function Home() {
-  const { user } = useGetUserData();
-  const { books } = useBookSearch();
+  const { allBooks, lendo, lidos, queroler, abandonados, isLoading } = useFetchBooksByUser();
+  const [value, setValue] = useState('1');
 
-  // to do - arrumar o layout que ta esticando
-  // to do - arrumar o corte que a div ta fazendo no carrousel
+  const itemsPerPage = 8; // Número de itens por página
+  const [page, setPage] = useState({
+    allBooks: 1,
+    lendo: 1,
+    lidos: 1,
+    queroler: 1,
+    abandonados: 1,
+  });
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  const handlePageChange = (category: string, event: ChangeEvent<unknown>, newPage: number) => {
+    setPage(prev => ({ ...prev, [category]: newPage }));
+  };
+
+  // Função para calcular os itens a serem exibidos com base na página atual
+  const paginatedItems = (items: BookData[], currentPage: number) => {
+    const indexLastItem = currentPage * itemsPerPage;
+    const indexFirstItem = indexLastItem - itemsPerPage;
+    return items.slice(indexFirstItem, indexLastItem);
+  };
+
+
   return (
+
     <>
       <Layout>
-        <CarrouselTitle>Últimas Leituras</CarrouselTitle>
-        {/* <button onClick={() => console.log(books)}>Teste</button> */}
-        <Carrousel>
-          <BookResume />
-          <BookResume />
-          <BookResume />
-          <BookResume />
-          <BookResume />
-          <BookResume />
-          <BookResume />
-          <BookResume />
-        </Carrousel>
-        {/* <Review /> */}
+        <TabContext value={value}>
+          <TabList sx={TabListStyle} onChange={handleChange}>
+            <Tab id="tab" sx={TabStyle} label="Todos" value='1' />
+            <Tab sx={TabStyle} label="Lendo" value='2' />
+            <Tab sx={TabStyle} label="Lidos" value='3' />
+            <Tab sx={TabStyle} label="Quero Ler" value='4' />
+            <Tab sx={TabStyle} label="Abandonados" value='5' />
+          </TabList>
+
+          <TabPanel sx={{ height: '100%' }} value='1'>
+            <Carrousel id="carrossel">
+              <BooksWrapper id="books-wrapper">
+                {paginatedItems(allBooks, page.allBooks).map((book) => (
+                  <BookResume id={book.bookId} myBookId={book.id} status={book.bookStatus} />
+                ))}
+              </BooksWrapper>
+              <Pagination
+                count={Math.ceil(allBooks.length / itemsPerPage)}
+                page={page.allBooks}
+                onChange={(event, newPage) => handlePageChange('allBooks', event, newPage)}
+              />
+            </Carrousel>
+          </TabPanel>
+
+          <TabPanel sx={{ height: '100%' }} value='2'>
+            <Carrousel id="carrossel">
+              <BooksWrapper id="books-wrapper">
+                {paginatedItems(lendo, page.lendo).map((book) => (
+                  <BookResume id={book.bookId} myBookId={book.id} status={book.bookStatus} />
+                ))}
+              </BooksWrapper>
+              <Pagination
+                count={Math.ceil(lendo.length / itemsPerPage)}
+                page={page.lendo}
+                onChange={(event, newPage) => handlePageChange('lendo', event, newPage)}
+              />
+            </Carrousel>
+          </TabPanel>
+
+          <TabPanel sx={{ height: '100%' }} value='3'>
+            <Carrousel id="carrossel">
+              <BooksWrapper id="books-wrapper">
+                {lidos.length > 0 ? lidos.map((book) => (
+                  <BookResume id={book.bookId} myBookId={book.id} status={book.bookStatus} />
+                )) : (<p>Você não tem livros nessa lista</p>)}
+              </BooksWrapper>
+              <Pagination
+                count={Math.ceil(lendo.length / itemsPerPage)}
+                page={page.lendo}
+                onChange={(event, newPage) => handlePageChange('lendo', event, newPage)}
+              />
+            </Carrousel>
+          </TabPanel>
+
+          <TabPanel sx={{ height: '100%' }} value='4'>
+            <Carrousel id="carrossel">
+              <BooksWrapper id="books-wrapper">
+                {queroler.length > 0 ? queroler.map((book) => (
+                  <BookResume id={book.bookId} myBookId={book.id} status={book.bookStatus} />
+                )) : (<p>Você não tem livros nessa lista</p>)}
+              </BooksWrapper>
+              <Pagination
+                count={Math.ceil(lendo.length / itemsPerPage)}
+                page={page.lendo}
+                onChange={(event, newPage) => handlePageChange('lendo', event, newPage)}
+              />
+            </Carrousel>
+          </TabPanel>
+
+          <TabPanel sx={{ height: '100%' }} value='5'>
+            <Carrousel>
+              <BooksWrapper id="books-wrapper">
+                {abandonados.length > 0 ? abandonados.map((book) => (
+                  <BookResume id={book.bookId} myBookId={book.id} status={book.bookStatus} />
+                )) : (<p>Você não tem livros nessa lista</p>)}
+              </BooksWrapper>
+              <Pagination
+                count={Math.ceil(lendo.length / itemsPerPage)}
+                page={page.lendo}
+                onChange={(event, newPage) => handlePageChange('lendo', event, newPage)}
+              />
+            </Carrousel>
+          </TabPanel>
+
+        </TabContext>
       </Layout>
     </>
   )
