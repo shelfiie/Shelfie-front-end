66850406479d25583b1f196941a5c60ee5c52454
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2024-07-19 13:10:45 -0300

    fix: voltando as configs anteriores

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { AuthProvider } from './api/context/auth.tsx'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
