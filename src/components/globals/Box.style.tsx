86c8880b478842ce1f546dd6335ae3670f66cd33
mayaspiek <maya.spieker@gmail.com>
2024-07-25 22:06:52 -0300

    style: design da progressao pronto

--- src/components/globals/Box.style.tsx
+++ src/components/globals/Box.style.tsx
@@ -7,14 +7,21 @@ interface BoxProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     shadowcolor?: string;
     padding?: string;
     borderRadius?: string;
+    width?: string;
+    display?: string;
+    flexDirection?: string;
+    gap?: string;
 }
 
 const Box = styled.div<BoxProps>`
+    display: ${props => props.display || null};
+    flex-direction: ${props => props.flexDirection || null};
+    gap: ${props => props.gap || null};
     border-radius: ${props => props.borderRadius || Theme.borders.radiusRound};
 
     height: max-content;
     text-align: center;
-    
+    width: ${props => props.width || "100%"};
     background-color: ${props => props.backgroundcolor};
     box-shadow: 9px 9px ${props => props.shadowcolor || "#000"};
     color: ${(props) => props.color || "#000"};
@@ -22,9 +29,9 @@ const Box = styled.div<BoxProps>`
     padding: ${(props) => props.padding};
 `
 
-export const BoxShadow = ({...props}) => {
-    return(
-        <Box 
+export const BoxShadow = ({ ...props }) => {
+    return (
+        <Box
             {...props}
         />
     )
