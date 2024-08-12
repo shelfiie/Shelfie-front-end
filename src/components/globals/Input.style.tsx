54d8cd2462a0dbe2779bd6cc5988361ed9945e0a
mayaspiek <maya.spieker@gmail.com>
2024-08-11 22:20:27 -0300

    style: variaveis inutilizadas e mudando input

--- src/components/globals/Input.style.tsx
+++ src/components/globals/Input.style.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import { Theme } from "../../styles/theme";
 
-export const Input = styled.input`
+const InputStyle = styled.input`
     border: ${Theme.borders.border2px} solid ${Theme.colors.deep};
     border-radius: ${Theme.borders.radiusRound};
     padding: 1.5rem;
     width: 100%;
-`
+`;
+
+export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+    return (
+        <InputStyle {...props} />
+    );
+};
