48e22e6538d0af2a14668925844e30601b205327
Mayara Spieker Carvalho <79992764+mayspiek@users.noreply.github.com>
2024-08-13 09:53:52 -0300

    cositas

--- src/pages/reviews/reviews-card.tsx
+++ src/pages/reviews/reviews-card.tsx
@@ -1,6 +1,6 @@
 import { Rating } from "@mui/material";
 import { BookData } from "../../types/bookData";
-import { BoxWrapper, Icons, ReviewDate, ReviewDetails, TitleRating } from "./reviews-box.styles";
+import { BoxWrapper, Icons, Like, LikeDetails, ReviewDate, ReviewDetails, TitleRating } from "./reviews-box.styles";
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
@@ -9,10 +9,19 @@ import { Theme } from "../../styles/theme";
 import { useState } from "react";
 import { ReviewModal } from "./edit-review";
 import { Heart } from "../../components/globals/Heart.style";
+import { useFetchLikesQuantityByReview } from "../../api/hooks/useFetchLikesQuantityByReview";
 
-export const ReviewsCard = ({ review, isEditable }: { review: BookData['reviews'][]; isEditable: boolean }) => {
+type ReviewsCardProps = {
+    review: BookData['reviews'];
+    isEditable: boolean;
+    isLikable: boolean;
+};
+
+export const ReviewsCard = ({ review, isEditable, isLikable }: ReviewsCardProps) => {
     const [selectedReview, setSelectedReview] = useState<BookData['reviews'] | null>(null);
     const [isEditOpen, setIsEditOpen] = useState(false);
+
+    const { likesQuantity } = useFetchLikesQuantityByReview(review?.id ?? '');
 
     const handleEditClick = (review: BookData['reviews']) => {
         setSelectedReview(review);
@@ -24,44 +33,43 @@ export const ReviewsCard = ({ review, isEditable }: { review: BookData['reviews'
         setSelectedReview(null);
     };
 
-    const sortedReviews = review.sort((a, b) => {
-        const dateA = a?.createdAt ? new Date(a.createdAt).getTime() : 0;
-        const dateB = b?.createdAt ? new Date(b.createdAt).getTime() : 0;
-        return dateB - dateA;
-    });
+
     return (
         <>
-            {sortedReviews && sortedReviews.map((review: BookData['reviews']) => (
-                <BoxWrapper id="box-wrapper">
-                    <img src={review?.thumbnailUrl ?? review?.smallThumbnailUrl} alt={review?.title} />
-                    <ReviewDetails id="review-details">
-                        <TitleRating id="title-rating">
-                            <p>{review?.title}</p>
-                            <Icons>
-                                <Rating
-                                    name="read-only"
-                                    value={review?.rating} readOnly
-                                    icon={<StarRoundedIcon fontSize="inherit" />}
-                                    emptyIcon={<StarBorderRoundedIcon style={{ opacity: 0.55 }} />}
-                                />
+            <BoxWrapper id="box-wrapper">
+                <img src={review?.thumbnailUrl ?? review?.smallThumbnailUrl} alt={review?.title} />
+                <ReviewDetails id="review-details">
+                    <TitleRating id="title-rating">
+                        <p>{review?.title}</p>
+                        <Icons>
+                            <Rating
+                                name="read-only"
+                                value={review?.rating} readOnly
+                                icon={<StarRoundedIcon fontSize="inherit" />}
+                                emptyIcon={<StarBorderRoundedIcon style={{ opacity: 0.55 }} />}
+                            />
 
-                                {isEditable &&
-                                    <a onClick={() => handleEditClick(review)}>
-                                        <EditRoundedIcon
-                                            sx={{ fill: `${Theme.colors.pink}` }} />
-                                    </a>
-                                }
+                            {isEditable &&
+                                <a onClick={() => handleEditClick(review)}>
+                                    <EditRoundedIcon
+                                        sx={{ fill: `${Theme.colors.pink}` }} />
+                                </a>
+                            }
 
-                            </Icons>
-                        </TitleRating>
-                        <p>{review?.review}</p>
-                        <div>
-                            <Heart reviewId={review?.id} />
-                            <ReviewDate id="review-date">{formatDate(review?.createdAt ?? '')}</ReviewDate>
-                        </div>
-                    </ReviewDetails>
-                </BoxWrapper>
-            ))}
+                        </Icons>
+                    </TitleRating>
+                    <p>{review?.review}</p>
+                    <Like>
+                        {isLikable &&
+                            <LikeDetails>
+                                <Heart type="review" reviewId={review && review.id} />
+                                <span>{likesQuantity}</span>
+                            </LikeDetails>
+                        }
+                        <ReviewDate id="review-date">{formatDate(review?.createdAt ?? '')}</ReviewDate>
+                    </Like>
+                </ReviewDetails>
+            </BoxWrapper>
             {selectedReview && (
                 <ReviewModal
                     isOpen={isEditOpen}
