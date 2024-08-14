5d9a37b7b38a9abee00475746e81e1367a3379a9
mayaspiek <maya.spieker@gmail.com>
2024-08-14 19:07:21 -0300

    margem login e registro

--- src/pages/login/login-styles.ts
+++ src/pages/login/login-styles.ts
@@ -8,7 +8,7 @@ const LoginDiv = styled(BoxShadow)`
 
     padding: ${Theme.margins.margin5rem};
     margin: 0 auto;
-    margin-top: 30rem;
+    margin-top: 15rem;
 
     > div{
         margin: ${Theme.margins.margin5rem} 0 ${Theme.margins.margin6rem} 0;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -61,6 +61,7 @@ export const Theme = {
         mobileL: '425px',
         tablet: '768px',
         laptop: '1024px',
+        laptopM: '1280px',
         laptopL: '1440px',
         desktopS: '1920px',
         desktop: '2560px'
