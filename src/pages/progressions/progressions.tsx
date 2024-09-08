d0ee7f914a9a688e33ff23c4bac9d08aacd652e0
mayaspiek <maya.spieker@gmail.com>
2024-09-08 17:35:42 -0300

    style: excluindo linhas de testes

--- src/pages/progressions/progressions.tsx
+++ src/pages/progressions/progressions.tsx
@@ -1,18 +1,12 @@
 import { useFetchAllProgressions } from "../../api/hooks/useFetchProgressions";
 import { Layout } from "../layout/layout";
 import { ProgressionsCard } from "./progressions-card";
-
 import { ProgressionsStyles } from "./progressions.styles";
 import { ProgressionSkeleton } from "./progressions-skeleton";
 import { NoItemsFound } from "../../components/globals/NoItemsFound";
-import { useEffect } from "react";
 
 export const Progressions = () => {
   const { progressions, loading } = useFetchAllProgressions();
-
-  useEffect(() => {
-    console.log(loading)
-  }, [loading]);
 
   return (
     <Layout>
